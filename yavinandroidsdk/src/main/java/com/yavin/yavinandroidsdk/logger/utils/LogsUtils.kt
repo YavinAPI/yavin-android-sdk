1c5ccf59af22ad7ece425e2f64ababe68f1abf54
Pierre Ghazal <pierre@yavin.com>
2022-12-13 22:59:52 +0100

    Add activity lifecycle callbacks to log

--- yavinandroidsdk/src/main/java/com/yavin/yavinandroidsdk/logger/utils/LogsUtils.kt
+++ yavinandroidsdk/src/main/java/com/yavin/yavinandroidsdk/logger/utils/LogsUtils.kt
@@ -2,23 +2,23 @@ package com.yavin.yavinandroidsdk.logger.utils
 
 object LogsUtils {
 
-    // We take the fifth index because the caller function (apart from logger functions) is five functions deeper in the stacktrace tree.
     fun getCallerInfo(thread: Thread): String {
         val stackTrace = thread.stackTrace
         return buildCallerInfo(stackTrace)
     }
 
     /**
-     * This function able log to determine the name of the calling function based on the stacktrace
+     * This function able log to determine the name of the calling function based on the stacktrace.
+     * We take the fifth index because the caller function (apart from logger functions) is five functions deeper in the stacktrace tree.
      */
     private fun buildCallerInfo(stackTrace: Array<StackTraceElement>): String {
         return try {
             val callerFunctionDepth = 5
             val element = stackTrace[callerFunctionDepth]
-            val fileName = element.className.substringAfterLast(".")
+            val className = element.className.substringAfterLast(".")
             val methodName = "${element.methodName}()"
             val line = element.lineNumber
-            "$fileName[$methodName:$line]"
+            "$className.[$methodName:$line]"
         } catch (e: Exception) {
             if (stackTrace.isNotEmpty()) stackTrace.last().methodName else "NoStackTrace"
         }
