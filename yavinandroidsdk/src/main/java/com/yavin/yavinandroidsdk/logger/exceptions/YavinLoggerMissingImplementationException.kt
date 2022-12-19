69d77cc3e61140af8f9b92b9ef15a7210e44950d
Pierre Ghazal <pierre@yavin.com>
2022-12-19 17:43:25 +0100

    Avoid throwing exception. Instead log in logcat

--- yavinandroidsdk/src/main/java/com/yavin/yavinandroidsdk/logger/exceptions/YavinLoggerMissingImplementationException.kt
+++
@@ -1,3 +0,0 @@
-package com.yavin.yavinandroidsdk.logger.exceptions
-
-class YavinLoggerMissingImplementationException constructor(override val message: String = "YavinLogger is configured with '.registerNavControllerDestinationChangeListener()' but Activity is not implemented YavinLoggerNavigableActivity") : Exception(message)
